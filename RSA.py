dd990f4b7a2378bc2f9c9a4be94382bf31356413
ShumsZ <67175740+ShumsZ@users.noreply.github.com>
2022-02-18 01:20:53 +0000

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -1,9 +1,10 @@
 #This is a very interesting assignment problem I worked on for a coursework a few months ago.
 
 #The question demonstrates the importance of Padding using RSA, without which, it is open to potential risks.
-#Here we are given a list of words ('english.txt') alsong with a ciphertext and our modulus N and public exponent e which I have hardcoded in.
+#Here we are given a list of words ('english.txt') along with a ciphertext and our modulus N and public exponent e which I have hardcoded in.
 #the modulus N is too large to factorise so we know 'p' and 'q' were good choices.
 #The trick here is to use the provided list of words and to run a for loop through them all and encrypt each word using the RSA encryption algorithm
+#Since the ciphertext we are given is a number, we need to convert the word into a number first and then encrypt it.
 #We then compare encrypted word with the ciphertext and the one that matches points to the original message.
 #Padding would add a layer of security and prevent this from happening
 
@@ -21,10 +22,11 @@ word_file.close()
 
 converted_list = []
 
+# Add each word to out empty array
 for element in tqdm(word_list):
     converted_list.append(element.strip())
 
-
+# run a for lop through each item in the array, encrypt it and then compare it to the given cipher text. If they match, we have our original text
 for word in tqdm(converted_list):
     word_a = word.encode('utf-8')
     hex = word_a.hex()
@@ -32,8 +34,7 @@ for word in tqdm(converted_list):
     decimal = str(hex_to_decimal)
     print(decimal)
     encrypted_word = int(decimal) ** exponent % N
-    # print(encrypted_word)
+    
     if encrypted_word == cipher:
         print(word)
-
 
